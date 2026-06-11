6e63b9df51e4afa50a7eb6c28b50f63010d764f2
jeremiasrivarola <jeremiasrivarola@ipvc.pt>
2026-06-11 12:18:44 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+//app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 app.use('/auth', authRoutes)
 app.use('/books', bookRoutes)
