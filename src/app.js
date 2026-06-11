104690b83e29bd8bf1e532a97bbefa8c7e079637
jeremiasrivarola <jeremiasrivarola@ipvc.pt>
2026-06-11 12:21:39 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const cors = require('cors')
 //const path = require('path')
 
-const authRoutes = require('./routes/auth.routes')
-const bookRoutes = require('./routes/book.routes')
-const statsRoutes = require('./routes/stats.routes')
-const userRoutes = require('./routes/user.routes')
+// const authRoutes = require('./routes/auth.routes')
+// const bookRoutes = require('./routes/book.routes')
+// const statsRoutes = require('./routes/stats.routes')
+// const userRoutes = require('./routes/user.routes')
 
 const app = express()
 
@@ -13,9 +13,9 @@ app.use(cors())
 app.use(express.json())
 //app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
-app.use('/auth', authRoutes)
-app.use('/books', bookRoutes)
-app.use('/users', userRoutes)
-app.use('/stats', statsRoutes)
+// app.use('/auth', authRoutes)
+// app.use('/books', bookRoutes)
+// app.use('/users', userRoutes)
+// app.use('/stats', statsRoutes)
 
 module.exports = app
