be9dd92fcbd267af4747bee0bca4b8f6b05a27f0
jeremiasrivarola <jeremiasrivarola@ipvc.pt>
2026-06-11 14:37:52 +0100

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,21 @@ const userRoutes = require('./routes/user.routes')
 
 const app = express()
 
-app.use(cors())
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  'http://localhost:5173'
+]
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true
+}))
 app.use(express.json())
 
 app.use('/auth', authRoutes)
